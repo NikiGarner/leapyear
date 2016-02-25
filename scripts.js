bd3cc00463c29cb40c45fd1cec7c0dccb150444d
NikiGarner <nbarfeild5@gmail.com>
2016-02-24 18:41:28 -0800

    make function for third behavior

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
 $(document).ready(function(){
-var leapYear = function(year){
-  return false;
-};
 
 var leapYear = function(year){
-  if(year % 4 === 0){
+  if(year % 4 === 0) && (year % 100 !== 0)){
     return true;
   } else{
     return false;
   }
 };
+
 });
